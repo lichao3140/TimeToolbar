b6d1902992b018fca704e3178eb3755ce17abed2
lichao3140 <396229938@qq.com>
2018-01-26 14:33:31 +0800

    Initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/apps/mohamed/customtoolbar/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package apps.mohamed.customtoolbar.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "apps.mohamed.customtoolbar.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
